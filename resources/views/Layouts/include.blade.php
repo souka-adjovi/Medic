8d633875f53d02db0d61ca4166c820741fc89f24
Souka_Alice <soukaalice@gmail.com>
2020-09-12 09:26:40 +0000

    Mise en place

---
+++ resources/views/Layouts/include.blade.php
@@ -0,0 +1,8 @@
+<head>
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+<link rel="stylesheet" href="{{asset('css/chocolat.css')}}">
+<link rel="stylesheet" href="{{asset('css/fontawesome-all.css')}}">
+<link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
+
+</head>
